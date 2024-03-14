57a091506c2c48781ff9dc13802f639f2f8593ad
ododo <148368269+protectinghome@users.noreply.github.com>
2024-03-14 11:45:06 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+    int a,b,c;
+    scanf("%d %d", &a, &b);
+    scanf("%d", &c);
+
+    a += (c/60);
+    b += (c%60);
+    if(b > 59)
+    {
+        a++;
+        b -= 60;
+    }
+
+    if(a > 23)
+    {
+        a -=24;
+    }
+
+    printf("%d %d", a, b);
+    return 0;
+}
