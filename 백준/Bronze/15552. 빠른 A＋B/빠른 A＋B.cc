fb637b03bccc72f4f800afa8d7fa7e9601926e0a
ododo <148368269+protectinghome@users.noreply.github.com>
2024-03-14 11:43:22 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 360 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,14 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+int main(void) {
+	int T,a,b;
+	scanf("%d", &T);
+	for (int t = 0; t < T; t++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("%d \n", a + b);
+	}
+	
+
+	return 0;
+}
