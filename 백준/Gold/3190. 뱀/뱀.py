13a653c40d24fc95b4e20a14aa07d0b7acbd272d
Dowon Lee <ldwlee03@gmail.com>
2025-08-19 20:42:34 +0900

    Update 뱀.py

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,62 +1,85 @@
 import sys
 from collections import deque
 
+# 보드의 크기 받기
 N = int(sys.stdin.readline())
+
+# 사과의 개수 받기
 K = int(sys.stdin.readline())
 
+# 보드판 설정
 board = [[0] * N for _ in range(N)]
 
-for _ in range(K):
-    R, C = map(int, sys.stdin.readline().split())
-    board[R - 1][C - 1] = 2  # 사과는 2로 표시
+# 사과 위치 설정
+for _ in range(K) :
+    row, columns = map(int, sys.stdin.readline().split())
+    board[row - 1][columns - 1] = 2
 
+# 뱀의 방향 변환 정보 받기
 L = int(sys.stdin.readline())
+
+# 뱀의 방향 변환 정보 저장
 directions = deque()
-for _ in range(L):
+for _ in range(L) :
     X, C = sys.stdin.readline().split()
     directions.append((int(X), C))
 
-# 뱀의 초기 위치와 방향
-snake = deque([(0, 0)])
-board[0][0] = 1  # 뱀의 몸통은 1로 표시
-time = 0
-x, y = 0, 0
+# 초기 위치
+x = 0
+y = 0
 
-# 동, 남, 서, 북
+# 초기 위치 1로 두기
+board[x][y] = 1
+
+# 뱀 위치 저장
+body = deque()
+body.append([x, y])
+
+# 동,서,남,북 방향 설정
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
-d = 0 # 초기 방향: 동쪽
 
-while True:
+# 초기 방향 설정
+d = 0
+
+# 시간 저장 변수
+time = 0
+
+# 시뮬레이션 시작
+while True :
+    # 시간 추가
     time += 1
-    
-    # 다음 머리 위치
-    nx = x + dx[d]
-    ny = y + dy[d]
 
-    # 게임 종료 조건 (벽 또는 자기 몸통에 부딪힘)
-    if not (0 <= nx < N and 0 <= ny < N and board[nx][ny] != 1):
+    # 뱀 이동
+    x += dx[d]
+    y += dy[d]
+
+    # 뱀 이동위치 저장
+    body.append([x, y])
+    
+    # 만약에 몸에 닿았거나 벽에 닿았을 때 시뮬레이션 중지
+    if x < 0 or x >= N or y < 0 or y >= N or board[x][y] == 1:
         break
     
-    # 사과가 있는 경우
-    if board[nx][ny] == 2:
-        board[nx][ny] = 1
-        snake.append((nx, ny))
-    # 사과가 없는 경우
-    else:
-        board[nx][ny] = 1
-        snake.append((nx, ny))
-        tail_x, tail_y = snake.popleft()
-        board[tail_x][tail_y] = 0
-
-    x, y = nx, ny
+    # 사과를 먹었을 때는 1로 설정해두기
+    if board[x][y] == 2 :
+        board[x][y] = 1
+    # 사과를 먹지 못했을 경우 꼬리 위치를 0으로 설정
+    else :
+        board[x][y] = 1
+        bodyp = body.popleft()
+        board[bodyp[0]][bodyp[1]] = 0
     
-    # 방향 전환 시간인지 확인
-    if directions and directions[0][0] == time:
-        X, C = directions.popleft()
-        if C == 'L':
-            d = (d - 1 + 4) % 4
-        else: # 'D'
-            d = (d + 1) % 4
+    # 만약에 방향 정보가 있고 시간이 다 되면 방향 옮기기
+    if directions and directions[0][0] == time :
+        xx, cc = directions.popleft()
+        if cc == 'D' :
+            d = d + 1
+        else :
+            d = d - 1
 
+        if d == -1 :
+            d = 3
+        if d == 4 :
+            d = 0
 print(time)
