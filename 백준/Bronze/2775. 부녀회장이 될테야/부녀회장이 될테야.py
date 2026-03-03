3fecc628dd1ffac8c7fe19ee86e680cefb906f84
Dowon Lee <ldwlee03@gmail.com>
2026-03-03 23:17:39 +0900

    Refactor 부녀회장이 될테야.py with comments
    Refactor code to improve readability and maintainability. Added comments explaining the logic behind the nested loop for calculating the number of residents.

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
+T = int(input()) 
 
 for _ in range(T) :
     K = int(input())
@@ -15,4 +15,10 @@ for _ in range(T) :
 
     print(floor[N-1])
 
+'''
+- 이중반복문을 이용한 덧셈 규칙
+먼저, 호수는 1부터 시작하므로 1~N(입력값)까지 floor 리스트에 넣는다.(0층은 1,2,3,4, ... , N)
+그 다음 1호를 제외한 나머지 호수값에 이전 호수값을 더한다.
+이를 K번 반복한 후 호수(N)의 값을 출력한다.
+'''
     
