60004fbea875d650d8c68a70a51219d7491efeed
Dowon Lee <ldwlee03@gmail.com>
2026-04-25 19:56:33 +0900

    [level 2] Title: 하노이의 탑, Time: 9.09 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -4,8 +4,11 @@ def solution(n):
         if n == 1 :
             answer.append([start, end])
             return
+        # 첫번째 기둥에서 (n-1) 원판들을 마지막 기둥을 거쳐 중간 기둥으로 옮긴다.
         hanoi(start, end, mid, n - 1)
+        # 마지막에 있던 원판을 첫번째 기둥에서 마지막 기둥으로 옮긴다.
         answer.append([start, end])
+        # 나머지 중간 기둥에 있던 (n-1)만큼의 원판을 첫번째 기둥을 거쳐 마지막 기둥으로 옮긴다.
         hanoi(mid, start, end, n - 1)
     hanoi(1,2,3,n)
     return answer
