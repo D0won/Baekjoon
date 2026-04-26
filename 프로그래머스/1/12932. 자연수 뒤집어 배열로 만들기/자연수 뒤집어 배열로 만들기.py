25e7009ee4c07f14c587b66fd8db3cfd9a2e8ca7
Dowon Lee <ldwlee03@gmail.com>
2026-04-26 12:50:09 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,6 +1,9 @@
 def solution(n):
     answer = []
-    nl = len(str(n))
+    nl = len(str(n)) # n의 길이를 구한다.
+    
+    # n의 길이만큼 10을 나눈 나머지를 answer에 추가하고
+    # 그리고 n을 10만큼 나눈다.
     for i in range(1, nl+1) :
         answer.append(n % (10))
         n //= 10
